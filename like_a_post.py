8412cd9755b36290409e2197afa865181e90a9a2
11shubhamsharma <sharmaaaa1994@gmail.com>
2017-07-06 15:51:57 +0530

    add if and else statement to check the request true or or not

--- like_a_post.py
+++ like_a_post.py
@@ -1,12 +1,15 @@
 import requests
 #import access token and basic url from constansts file
 from constants import APP_ACCESS_TOKEN,BASE_URL
-
-
-
+#import get_post_id from get _post_id
+from get_post_id import get_post_id
 def like_a_post(insta_username):
   media_id = get_post_id(insta_username)
   request_url = (BASE_URL + 'media/%s/likes') % (media_id)
   payload = {"access_token": APP_ACCESS_TOKEN}
   print 'POST request url : %s' % (request_url)
-  post_a_like = requests.post(request_url, payload).json(
+  post_a_like = requests.post(request_url, payload).json()
+  if post_a_like['meta']['code'] == 200:
+      print 'Like was successful!'
+  else:
+      print 'Your like was unsuccessful. Try again!'
