24038474f80fed7221aec9624dc0a5ef4389be48
11shubhamsharma <sharmaaaa1994@gmail.com>
2017-07-06 16:12:39 +0530

    add if else statement to check the request is true or not

--- delete_negative_comment.py
+++ delete_negative_comment.py
@@ -9,3 +9,10 @@ def delete_negative_comment(insta_username):
   request_url = (BASE_URL + 'media/%s/comments/?access_token=%s') % (media_id, APP_ACCESS_TOKEN)
   print 'GET request url : %s' % (request_url)
   comment_info = requests.get(request_url).json()
+  if comment_info['meta']['code'] == 200:
+      if len(comment_info['data']):
+      # code to delete a comment
+      else:
+          print 'There are no existing comments on the post!'
+  else:
+      print 'Status code other than 200 received!'
