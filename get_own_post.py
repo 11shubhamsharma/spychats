74b1f94870f8c3e7c6c566f7526f0d9d8c66bf7b
11shubhamsharma <sharmaaaa1994@gmail.com>
2017-07-11 19:48:47 +0530

    define the git_own_post function in this file

--- get_own_post.py
+++ get_own_post.py
@@ -5,7 +5,7 @@ from constants import APP_ACCESS_TOKEN,BASE_URL
 # define get_own_post function
 def get_own_post():
   request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
-  print 'GET request url : %s' % (request_url)
+  print ('GET request url : %s' % (request_url))
   own_media = requests.get(request_url).json()
   if own_media['meta']['code'] == 200:
       if len(own_media['data']):
