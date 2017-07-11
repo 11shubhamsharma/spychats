fd98daf09525199dca37244e9a3b7408806f9e8d
11shubhamsharma <sharmaaaa1994@gmail.com>
2017-07-11 20:03:28 +0530

    define  get_user_id function in this file

--- get_user_id.py
+++ get_user_id.py
@@ -4,7 +4,7 @@ from constants import APP_ACCESS_TOKEN,BASE_URL
 # define get_user_id function
 def get_user_id(insta_username):
   request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
-  print 'GET request url : %s' % (request_url)
+  print ('GET request url : %s' % (request_url))
   user_info = requests.get(request_url).json()
   # print user_info
   if user_info['meta']['code'] == 200:
@@ -13,5 +13,5 @@ def get_user_id(insta_username):
       else:
           return None
   else:
-      print 'Status code other than 200 received!'
+      print ('Status code other than 200 received!')
       exit()
