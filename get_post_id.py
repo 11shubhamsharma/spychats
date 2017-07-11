31436b6e3d9d8a6e2630467f07a8f0da5775b204
11shubhamsharma <sharmaaaa1994@gmail.com>
2017-07-11 19:52:25 +0530

    add the get _post _id function in this file

--- get_post_id.py
+++ get_post_id.py
@@ -6,7 +6,7 @@ from constants import APP_ACCESS_TOKEN,BASE_URL
 def get_post_id(insta_username):
     user_id = get_user_id(insta_username)
     if user_id == None:
-        print 'User does not exist!'
+        print ('User does not exist!')
         exit()
     request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
@@ -16,8 +16,8 @@ def get_post_id(insta_username):
         if len(user_media['data']):
             return user_media['data'][0]['id']
         else:
-            print 'There is no recent post of the user!'
+            print ('There is no recent post of the user!')
             exit()
     else:
-        print 'Status code other than 200 received!'
+        print ('Status code other than 200 received!')
         exit()
