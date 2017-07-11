b194f429acca71203d02dfcf8653fd992def0836
11shubhamsharma <sharmaaaa1994@gmail.com>
2017-07-11 20:07:21 +0530

    define list_a_commit function in this file

--- list_a_comment.py
+++ list_a_comment.py
@@ -5,12 +5,12 @@ insta_username="shubhamsharma.una"
 def list_a_comment(insta_username):
     media_id=get_post_id(insta_username)
     request_url=(BASE_URL +'media/%s/comments?ACCESS_TOKEN=%s')%(media_id,APP_ACCESS_TOKEN)
-    print"GET request url: %s" % (request_url)
+    print("GET request url: %s" % (request_url))
     if post_a_comment()["meta"]["code"]==200:
-       print"media with media id '{}' is comment by following users :".format(media_id)
+       print("media with media id '{}' is comment by following users :".format(media_id))
        for (index,users_comments)in enumerate(post_a_comment()["data"]):
-          print"%d. %s (%s) - %d" % (index+1,users_comments["full_name"],users_comments["id"],users_comments["username"])
+          print("%d. %s (%s) - %d" % (index+1,users_comments["full_name"],users_comments["id"],users_comments["username"]))
     else:
-        print "wrong input  plzz try again"
+        print ("wrong input  plzz try again")
 
 list_a_comment(insta_username)
